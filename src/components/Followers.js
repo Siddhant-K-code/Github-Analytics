f19fc89e966ed9c092d60a119799fb801442f6d3
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-08-03 10:30:41 +0530

    User & Follower Component added
    User & Follower Component added, next is various types of charts

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
+import React from "react";
+import { GithubContext } from "../context/context";
+import styled from "styled-components";
 
 const Followers = () => {
-  return <h2>followers component</h2>;
+  const { followers } = React.useContext(GithubContext);
+
+  return (
+    <Wrapper>
+      <div className="followers">
+        {followers.map((follower, index) => {
+          const { avatar_url: img, html_url, login } = follower;
+          return (
+            <article key={index}>
+              <img src={img} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>{html_url}</a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.article`
@@ -14,7 +33,7 @@ const Wrapper = styled.article`
   position: relative;
 
   &::before {
-    content: ' followers';
+    content: " followers";
     position: absolute;
     top: 0;
     left: 0;
@@ -29,13 +48,20 @@ const Wrapper = styled.article`
     font-size: 1rem;
   }
   .followers {
-    overflow: scroll;
-    height: 260px;
+    overflow-y: scroll;
+    height: 320px;
     display: grid;
     grid-template-rows: repeat(auto-fill, minmax(45px, 1fr));
     gap: 1.25rem 1rem;
     padding: 1rem 2rem;
   }
+  /*  .scrollbar {
+    display: inline-block;
+    overflow: hidden;
+    position: relative;
+    height: 200px;
+    width: 380px;
+  } */
   article {
     transition: var(--transition);
     padding: 0.15rem 0.5rem;
