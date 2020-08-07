77a728a8cc67e58488f55ba83dbd62dfab5bd699
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-08-07 19:08:55 +0530

    repos info showing in consoles
    repos info showing in consoles, dynamic data on page is left and then ahead

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,18 +4,29 @@ import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
+
   let languages = repos.reduce((total, item) => {
     const { language } = item;
     if (!language) return total;
     if (!total[language]) {
-      total[language] = 1;
+      total[language] = { label: language, value: 1 };
     } else {
-      total[language] = 30;
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
     }
-    console.log(language);
 
     return total;
   }, {});
+
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 7);
+  /*   console.log(languages); */
+
   const chartData = [
     {
       label: "HTML",
